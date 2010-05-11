66d63c46171ea3d6151d4997d712f4939f1e32ef
miffff <miffff@f8e1cdb3-d679-e7da-ce80-8a6f8a1ab45e>
2010-05-11 20:53:58 +0000

    Some sort of swing thing.

---
+++ branches/GuiExperiments/NanobotBuilder/src/gui/MyPanel.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui;
+
+import java.awt.*;
+import java.util.Map;
+import javax.swing.*;
+import world.AbstractNanobot;
+import world.GridCell;
+
+/**
+ *
+ * @author Wawrzyniec
+ */
+
+public class MyPanel extends JPanel
+{
+
+    int i;
+    Object lock = new Object();
+    
+   public MyPanel()
+   {
+       i = 0;
+      setPreferredSize(new Dimension(800, 600));
+   }
+
+   public Map<AbstractNanobot, GridCell> worldMap;
+
+   @Override
+   public void paintComponent(Graphics g)
+   {
+      super.paintComponent(g);
+      Graphics2D g2d = (Graphics2D)g;
+      g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if(worldMap != null){
+          for(AbstractNanobot anb: worldMap.keySet()){
+            g2d.setPaint(Color.RED);
+            GridCell cell = worldMap.get(anb);
+            Rectangle rect = new Rectangle(10 + 20*cell.getX(), 20 + 20*cell.getY(), 18, 18);
+            g2d.fill(rect);
+          }
+        }
+      System.out.println("Painting Component.");
+      i++;
+   }
+}
