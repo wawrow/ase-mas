3a1a2316b4f68fb7686e733649eeb74f063f2c25
miffff <miffff@f8e1cdb3-d679-e7da-ce80-8a6f8a1ab45e>
2010-05-12 00:13:21 +0000

    Few small changes to support colors.

--- NanobotBuilder/src/gui/MyPanel.java
+++ NanobotBuilder/src/gui/MyPanel.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gui;
 
 import java.awt.*;
@@ -15,36 +14,36 @@ import world.GridCell;
  *
  * @author Wawrzyniec
  */
-
-public class MyPanel extends JPanel
-{
+public class MyPanel extends JPanel {
 
     int i;
     Object lock = new Object();
-    
-   public MyPanel()
-   {
-       i = 0;
-      setPreferredSize(new Dimension(800, 600));
-   }
 
-   public Map<AbstractNanobot, GridCell> worldMap;
+    public MyPanel() {
+        i = 0;
+        setPreferredSize(new Dimension(800, 600));
+    }
+    public Map<AbstractNanobot, GridCell> worldMap;
 
-   @Override
-   public void paintComponent(Graphics g)
-   {
-      super.paintComponent(g);
-      Graphics2D g2d = (Graphics2D)g;
-      g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        if(worldMap != null){
-          for(AbstractNanobot anb: worldMap.keySet()){
-            g2d.setPaint(Color.RED);
-            GridCell cell = worldMap.get(anb);
-            Rectangle rect = new Rectangle(10 + 20*cell.getX(), 20 + 20*cell.getY(), 18, 18);
-            g2d.fill(rect);
-          }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (worldMap != null) {
+            for (AbstractNanobot anb : worldMap.keySet()) {
+                switch(anb.getType()){
+                    case 1: g2d.setPaint(Color.RED); break;
+                    case 2: g2d.setPaint(Color.BLUE); break;
+                    case 3: g2d.setPaint(Color.green); break;
+                    default: g2d.setPaint(Color.BLACK); break;
+                }
+                GridCell cell = worldMap.get(anb);
+                Rectangle rect = new Rectangle(10 + 20 * cell.getX(), 20 + 20 * cell.getY(), 18, 18);
+                g2d.fill(rect);
+            }
         }
-      System.out.println("Painting Component.");
-      i++;
-   }
+        System.out.println("Painting Component.");
+        i++;
+    }
 }
