2c1f4a48634e8c937904dbac4c9da86724a02e31
fergusmccann@yahoo.com <fergusmccann@yahoo.com@f8e1cdb3-d679-e7da-ce80-8a6f8a1ab45e>
2010-05-14 03:25:07 +0000

    Tiny typo fixed which caused some problems - still haven't got welding working - not much more to get it working though

--- NanobotBuilder/src/actuator/GotWelderTask.java
+++ NanobotBuilder/src/actuator/GotWelderTask.java
@@ -10,6 +10,7 @@ public class GotWelderTask extends Actuator {
         String y1 = action.argAt(2).toString();
         String x2 = action.argAt(3).toString();
         String y2 = action.argAt(4).toString();
+        System.out.println(agent.getName() + ": got new task from " + agentName + " target weld endpoints=(" + x1 + "," + y1 + ") (" + x2 + "," + y2 + ")");        
 
         // Target for a weld is in the middle of the two endpoints
         Integer targetX = (Integer.parseInt(x1) + Integer.parseInt(x2)) / 2;
@@ -19,7 +20,6 @@ public class GotWelderTask extends Actuator {
         retractBelief("ALWAYS(BELIEF(state(weldEndpoints(?x1, ?y1, ?x2, ?y2))))");
         adoptBelief("ALWAYS(BELIEF(state(target(" + targetX + "," + targetY + "))))");
         adoptBelief("ALWAYS(BELIEF(state(weldEndpoints(" + x1 + "," + y1 + "," + x2 + "," + y2 + "))))");
-        System.out.println(agent.getName() + ": got new task from " + agentName + " target weld endpoints=(" + x1 + "," + y1 + ") (" + x2 + "," + y2 + ")");
         return true;
     }
 }
